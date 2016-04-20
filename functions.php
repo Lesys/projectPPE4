1cd2971e5a5448dd8c4666ce4e78bb70091fba7c
Lesys <alexis.widmer@bts-malraux.net>
2016-04-20 13:30:12 +0200

    Add function for the connection with the DB (not ended) and wiki (DB tables)

---
+++ functions.php
@@ -0,0 +1,32 @@
+<?php
+function connectionDB() {
+    $PARAMHost = '172.16.99.3'; //Server path
+    $PARAMPort = '3306'; //Server port
+    $PARAMDBName = 'examen.bidault-widmer'; //DataBase name
+    $PARAMUser = 'a.widmer'; //User login
+    $PARAMPassword = 'passe'; //User Password
+
+    $connection = new PDO('mysql:host='.$PARAMHost.';port='.$PARAMPort.';dbname='.$PARAMDBName, $PARAMUser, $PARAMPassword,
+        array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+
+    return $connection;
+}
+
+function queryDBSmartphone() {
+	$request = "SELECT * FROM reunion ORDER BY date_reunion;"; //Query for the SQL query
+    
+    $resultat = $co->query($request); //All fetchs in the $resultat variable
+
+    while ($row = $resultat->fetch()) { //For each row in the result of the query
+        $return[$row["id_reunion"]] = array(
+        	"id_reunion"=>$row["id_reunion"],
+        	"date_reunion"=>$row["date_reunion"],
+        	"intitule_reunion"=>$row["intitule_reunion"],
+        	"descriptif_reunion"=>$row["descriptif_reunion"]);
+    }
+
+    $resultat->closeCursor();
+
+    return $
+}
+?>
---
+++ wiki.rst
@@ -0,0 +1,17 @@
+DataBase :
+Synthaxe pour le schéma relationnel :
+
+- nom_table(parametre_1, parametre_2 ..., parametre_X);
+
+                parametre_1(type[, AUTO_INCREMENT]): Description du paramètre (Clé primaire, Clé étrangère (avec référence)...)
+                parametre_2(type): La même chose....
+
+Schéma relationnel :
+
+- reunion(id_reunion, date_reunion, intitule_reunion, descriptif_reunion);
+
+                id_reunion(int, AUTO_INCREMENT): Clé primaire de la table "reunion"
+                date_reunion(datetime): La date et l'heure à laquelle la réunion a lieu
+                intitule_reunion(text): Titre (ou sujet principal) de la réunion (ex: "Réunion sur les comptes et le budget de l'entreprise")
+                descriptif_reunion(text): Informations complémentaires concernant la réunion (ex: autres sujets que le principal)
+
