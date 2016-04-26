184ce20289d379835b0f40d90c1eef2d545c5b22
Lesys <alexis.widmer@bts-malraux.net>
2016-04-26 16:10:01 +0200

    Test php site with data in DB

--- php/index.php
+++ php/index.php
@@ -1,6 +1,20 @@
-<?php include "view/header.html"; ?>
+<?php include "view/header.html"; 
+
+	include "useful/functions.php";
+
+	$reunions = queryDB();
+	?>
 	
 	<div class="title1">Calendar Project - Bidault Bastien / Widmer Alexis</div>
 
+	<div class="allReunions">All reunions:<pre>
+
+	<?php 
+		foreach ($reunions as $reunion) {
+			echo $reunion[date_reunion]."<pre>Reunion for ".$reunion[intitule_reunion].". Take ".$reunion[duree_estimee_reunion]." at least for this time.</pre>";
+		}
+
+
+	?>
 
 <?php include "view/footer.html"; ?>
