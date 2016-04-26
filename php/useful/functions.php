49a17aa306e1b1be2c0da97ef1178d14a64dec92
Lesys <alexis.widmer@bts-malraux.net>
2016-04-26 16:47:09 +0200

    Working catching all data from the DB on index.php

--- php/useful/functions.php
+++ php/useful/functions.php
@@ -12,22 +12,23 @@ function connectionDB() {
     return $connection;
 }
 
-function queryDB() {
+function queryDB() {
 	$co = connectionDB();
-	$request = "SELECT * FROM reunion ORDER BY date_reunion;"; //Query for the SQL query
+	$request = "SELECT * FROM reunion ORDER BY date_reunion DESC;"; //Query for the SQL query
     
     $resultat = $co->query($request); //All fetchs in the $resultat variable
 
     while ($row = $resultat->fetch()) { //For each row in the result of the query
-        $return[$row["id_reunion"]] = array(
+        $return[$row["id_reunion"] - 1] = array(
         	"id_reunion"=>$row["id_reunion"],
         	"date_reunion"=>$row["date_reunion"],
+            "duree_estimee_reunion"=>$row["duree_estimee_reunion"],
         	"intitule_reunion"=>$row["intitule_reunion"],
         	"descriptif_reunion"=>$row["descriptif_reunion"]);
     }
 
     $resultat->closeCursor();
 
-    return json_encode($return);
+    return $return;
 }
 ?>
